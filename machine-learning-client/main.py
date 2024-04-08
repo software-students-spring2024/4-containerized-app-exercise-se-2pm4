5820d594ce0e82b3548382d5cbf9736eb77760c8
Tango117 <bjt312@nyu.edu>
2024-04-08 13:57:57 -0400

    machine-learning-client black fix

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -3,7 +3,7 @@
 from deepface import DeepFace
 
 face_analysis = DeepFace.analyze(img_path="./img/man.jpg")
-print('Gender:', face_analysis[0]['dominant_gender'])
-print('Age:', face_analysis[0]['age'])
-print('Race:', face_analysis[0]['dominant_race'])
-print('Emotion:', face_analysis[0]['dominant_emotion'])
+print("Gender:", face_analysis[0]["dominant_gender"])
+print("Age:", face_analysis[0]["age"])
+print("Race:", face_analysis[0]["dominant_race"])
+print("Emotion:", face_analysis[0]["dominant_emotion"])
