9fc4dfaa5c61f9646bf208cffe44b177ef51b0eb
minjae07206 <minjaekor@gmail.com>
2024-04-10 19:12:29 +0400

    connected database to webapp

--- web-app/app.py
+++ web-app/app.py
@@ -14,7 +14,7 @@ os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 # load environment variables
 #mongo_uri = os.environ.get("MONGO_URI")
 flask_port = os.environ.get("FLASK_RUN_PORT")
-MONGO_HOST = os.environ.get("DB_HOST", "mongodb_server")  # This should match the service name in the Docker Compose file
+MONGO_HOST = os.environ.get("DB_HOST", "mongodb_server")
 
 # Load environment variables for MongoDB connection
 MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
