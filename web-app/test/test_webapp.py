12ca6c1f9d7913866ae56e8d26a58eb2adae135c
minjae07206 <minjaekor@gmail.com>
2024-04-13 20:31:44 +0400

    changed test

--- web-app/test/test_webapp.py
+++ web-app/test/test_webapp.py
@@ -1,35 +1,22 @@
 """
-Module for testing Web-app functionalities.
+Module for testing DeepFace functionalities.
 """
 
-import os
 import pytest
 from flask import Flask
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import app
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "static/uploads"
-os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
-# load environment variables
-# mongo_uri = os.environ.get("MONGO_URI")
-flask_port = os.environ.get("FLASK_RUN_PORT")
-MONGO_HOST = os.environ.get("DB_HOST", "mongodb_server")
-
-# Load environment variables for MongoDB connection
-MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
-MONGO_DB = os.environ.get("MONGO_DB", "image_emotion_db")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-mongo_uri = f"mongodb://{DB_USER}:{DB_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}"
-client = MongoClient(mongo_uri)
-# db = client["image_emotion_db"]
-db = client.get_database(MONGO_DB)
+# Initialize MongoDB client and collection
+client = MongoClient("mongodb://localhost:27017/")
+db = client.get_database("image_emotion_db")
 images_collection = db["images"]
 
-
-class Tests:
+class TestDeepFace:
     """
     Test class for DeepFace functionalities.
     """
@@ -37,16 +24,16 @@ class Tests:
     @pytest.fixture
     def test_home_get(self):
         """
-        Test the app GET home route at launch
+        Test the GET request to the home page.
         """
         response = app.test_client().get("/")
         assert response.status_code == 200
 
     def test_home_post(self):
         """
-        Test does the app respond to a given image.
+        Test the POST request to the home page with a sample image.
         """
-        test_img_path = "../img/man.jpg"
+        test_img_path = "path/to/sample/image.jpg"
         with open(test_img_path, "rb") as image_file:
             image_data = image_file.read()
         response = app.test_client().post(
@@ -58,19 +45,20 @@ class Tests:
 
     def test_gallery_route(self):
         """
-        Test the app GET gallery route at launch
+        Test the GET request to the gallery page.
         """
         response = app.test_client().get("/gallery")
         assert response.status_code == 200
 
     def test_check_status(self):
         """
-        Test the whether image id are generated correctly
+        Test the whether image id are generated correctly.
         """
         sample_id = "sample_id"
         images_collection.insert_one(
             {"_id": sample_id, "processed": False, "emotion": "happy"}
         )
         response = images_collection.find_one({"_id": ObjectId(sample_id)})
-        assert response == 200
+        assert response["_id"] == sample_id
         images_collection.delete_many({})
+
