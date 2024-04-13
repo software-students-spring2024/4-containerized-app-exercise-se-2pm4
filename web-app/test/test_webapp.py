2f690162ab5fee52b3422f82e4c2a108c3c42b5b
yl7408 <yl7408@nyu.edu>
2024-04-13 00:06:59 -0400

    reformatted

--- web-app/test/test_webapp.py
+++ web-app/test/test_webapp.py
@@ -9,6 +9,7 @@ import pytest
 from werkzeug.utils import secure_filename
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "static/uploads"
 os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
@@ -28,6 +29,8 @@ client = MongoClient(mongo_uri)
 # db = client["image_emotion_db"]
 db = client.get_database(MONGO_DB)
 images_collection = db["images"]
+
+
 class Tests:
     """
     Test class for DeepFace functionalities.
@@ -35,28 +38,37 @@ class Tests:
 
     @pytest.fixture
     def test_home_get(client):
-        response = client.get('/')
+        response = client.get("/")
         assert response.status_code == 200
+
     @pytest.fixture
     def test_home_post(client):
         """
         Test does the app respond to a given image.
         """
-        test_img_path = "/Users/yiweiluo/4-containerized-app-exercise-se-2pm4/web-app/img/man.jpg"
-        with open(test_img_path, 'rb') as image_file:
+        test_img_path = (
+            "/Users/yiweiluo/4-containerized-app-exercise-se-2pm4/web-app/img/man.jpg"
+        )
+        with open(test_img_path, "rb") as image_file:
             image_data = image_file.read()
-        response = client.post('/', data = {'image': (image_data, 'man.jpg')}, content_type='multipart/form-data')
+        response = client.post(
+            "/",
+            data={"image": (image_data, "man.jpg")},
+            content_type="multipart/form-data",
+        )
         assert response.status_code == 200
+
     @pytest.fixture
     def test_gallery_route(client):
-        response = client.get('/gallery')
+        response = client.get("/gallery")
         assert response.status_code == 200
+
     @pytest.fixture
     def test_check_status(client):
-        sample_id = 'sample_id'
-        images_collection.insert_one({"_id": sample_id, "processed": False, "emotion": "happy"})
+        sample_id = "sample_id"
+        images_collection.insert_one(
+            {"_id": sample_id, "processed": False, "emotion": "happy"}
+        )
         response = images_collection.find_one({"_id": ObjectId(sample_id)})
         assert response == 200
         images_collection.delete_many({})
-        
-
